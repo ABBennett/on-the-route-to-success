653e8d28eb2e5b36f62390df612e8f64e623a5a3
AlexBennett <alexanderbondbennett@gmail.com>
2015-12-13 15:25:19 -0500

    train routes functioning, train tests passing

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151213200559) do
+ActiveRecord::Schema.define(version: 20151213201708) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,12 @@ ActiveRecord::Schema.define(version: 20151213200559) do
   end
 
   create_table "horses", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "trains", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
